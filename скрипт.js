234417662ef40ac30505a7e00c345856a897d1a1
SeregaTitan <158318874+SeregaTitan@users.noreply.github.com>
2024-01-31 20:28:42 +0300

    Add files via upload

---
+++ скрипт.js
@@ -0,0 +1,7 @@
+// script.js
+document.getElementById("reservation-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // Здесь можно добавить код для обработки бронирования
+    alert("Спасибо! Ваш столик забронирован.");
+});
